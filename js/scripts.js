e3525af548316650c248ac294555fcb20547e597
KelStalker <kelstalker@hotmail.com>
2020-03-02 15:56:53 -0700

    EOD day one of Java instruction

--- js/scripts.js
+++ js/scripts.js
@@ -56,18 +56,18 @@ function addAFace(){
  function exercise1(){
     for(i = 1; i < 100; i++)
 
-        if(i%3==1 && i%5==1)
+        if(i%3!=0 && i%5!=0)
             console.log(i);
+
+            else if(i%5==0 && i%3==0)
+            console.log('FizzBuzz');
         
         else if(i%3==0)
             console.log('Fizz');
         
-        else if(i%5==0 && i%3==1){
-        console.log('Buzz');
-        }
-        else if(i%5==0 && i%3==0){
-            console.log('FizzBuzz');
-        }
+        else if(i%5==0)
+            console.log('Buzz');
+
     }
     exercise1();
 
