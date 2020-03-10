c8659b871c0b56e9b1ffb91c88111cf603887136
KelStalker <kelstalker@hotmail.com>
2020-03-10 14:59:04 -0600

    working for now with one guess only

--- js/hangman_exercise_day6.js
+++ js/hangman_exercise_day6.js
@@ -1,25 +1,17 @@
 //  First attempt at making hangman
 
 
-function game() {
-
-}
-
-
-
 function hangmanGame(word, guesses) {
 
     this.dead = false;
     this.word = word;
     this.guesses = guesses;
     this.truthIndex = []; //Show or don't show the word to the user.
-    //Eg: "truck".
-    //Eg: [ false, false, false, false, false];
-    //Eg: ______
-    //
-    //Eg2: "truck"
-    // [true, false, false, false, false]
-    // t_____
+
+
+    // while (this.guesses != 0){ //Iterates x number of guesses, as passed in hangmanGame call
+
+
     for (var i = 0; i < word.length; i++) {
         this.truthIndex[i] = false;
     }
@@ -35,22 +27,36 @@ function hangmanGame(word, guesses) {
             }
         }
 
+        var isWinner = true;
+
         var str = "";
         for (var i = 0; i < word.length; i++) {
             if (this.truthIndex[i] == false) {
                 // console.log("_ ");
                 str = str + "_ ";
+                isWinner = false;
             }
             else {
                 // console.log(word[i]+" ");
                 str = str + word[i]+" ";
             }
         }
-        console.log(str);
+        console.log(str+" You have "+guesses+" guesses left");
 
+        function checkIsWinner(isWinner){
+            if (isWinner){
+                console.log("CONGRATULATIONS!")
+                this.guesses = 0;
+            }
+
+        }
+        checkIsWinner(isWinner);
     }
-
+    
+// do {
     this.userGuess();
+// } while (this.guesses != 0); 
+
 }
 
 game = new hangmanGame("truck", 5);
