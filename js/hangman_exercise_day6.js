0e02a33576dd0a384451dea8bad944a3e9d066c9
KelStalker <kelstalker@hotmail.com>
2020-03-11 09:42:34 -0600

    encapsulated processes and documented each step for easier reference for myself and anyone else reading my code

--- js/hangman_exercise_day6.js
+++ js/hangman_exercise_day6.js
@@ -4,40 +4,44 @@ function hangmanGame(word, guesses) {
     this.word = word;
     this.guesses = guesses;
     this.truthIndex = []; //Show or don't show the word to the user.
-    this.isWinner = true
+    this.isWinner = false
 
     // while (this.guesses != 0){ //Iterates x number of guesses, as passed in hangmanGame call
 
 
+    // truthIndex is a bool to indicate if a letter in the word has been found or not
+    // set the 'truthIndex' for each letter in the word to false
     for (var i = 0; i < word.length; i++) {
         this.truthIndex[i] = false;
     }
 
+
+    //Get user Input
     this.userGuess = function (letter) {
         letter = prompt("Mke a guess");
 
+        //Examine each index for letters in word and if the userGuess matches, set truthIndex to true for that letter
         for (var i = 0; i < word.length; i++) {
             if (letter === word[i]) {
                 this.truthIndex[i] = true;
-            } else {
-                this.guesses--;
+            }
+        }
+     }
+
+    // Build a string of either letters guessed, or underscores as place holders for letters that have not been guessed yet.
+    var str = "";
+    for (var i = 0; i < word.length; i++) {
+        if (this.truthIndex[i] == false) {
+            str = str + "_ ";
+        else {
+            str = str + word[i]+" ";
             }
         }
 
-        var str = "";
-        for (var i = 0; i < word.length; i++) {
-            if (this.truthIndex[i] == false) {
-                str = str + "_ ";
-                this.isWinner = false;
-            }
-            else {
-                // console.log(word[i]+" ");
-                str = str + word[i]+" ";
-            }
-        }
-        console.log(str+" You have "+this.guesses+" guesses left");
+    
+        
 
-
+        this.isWinner = false;
             if (this.isWinner){
                 console.log("CONGRATULATIONS!")
                 this.guesses = 0;
@@ -48,6 +52,12 @@ function hangmanGame(word, guesses) {
     }
 }
 
+    if (found == false){
+        guesses = guesses--
+    }
+
+
+    console.log(str+" You have "+this.guesses+" guesses left");
 game = new hangmanGame("truck", 5);
 
 
